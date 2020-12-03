1e91d03bf1d552264746a242cc7c0dd8d101ce51
qlRitscher <ritscher@skouz.de>
2020-12-03 12:03:53 +0100

    Icon Changes

--- Wetter.js
+++ Wetter.js
@@ -1,8 +1,137 @@
-let baseURL = "http://localhost:3030/";
-
 let tmpl = document.createElement('template');
 tmpl.innerHTML = `
-<link rel="stylesheet" type="text/css" href= "`+ baseURL + `Wetter.css" />
+<style>
+:host{
+    max-width: 600px;
+    display:block;
+    margin-left:auto;
+    margin-right:auto;
+}
+#weather{
+    display:flex;
+    position: relative;
+    flex-wrap: wrap;
+    background:var(--background, #fff);
+}
+.btn-group{
+    display: flex;
+    justify-content: space-between;
+}
+.btn-group button{
+    border:none;
+    width:100%;
+    padding:1em;
+    font-family: Lato, sans-serif;
+    background:var(--background-deactived,#f2f2f2)
+}
+.btn-group button.active{
+    background: var(--background,#fff);
+}
+daily-forecast{
+    background: var(--background,#fff);
+    padding:5%;
+    box-sizing: border-box;
+    display:flex;
+    justify-content: center;
+    order:2;
+}
+daily-forecast:not(#show){
+    cursor:pointer;
+}
+daily-forecast.chosen{
+    background:var(--background-chosen, #dff0ff);
+}
+.three daily-forecast:not(#show)
+{
+    width:33.3%;
+}
+
+.five daily-forecast:not(#show)
+{
+    width:20%;
+}
+
+.eight daily-forecast:not(#show)
+{
+    width:25%;
+}
+
+.current daily-forecast:not(#show){
+    display:none;
+}
+daily-forecast#show{
+    order:1;
+    width:100%!important;
+    height:auto;
+}
+.day{
+    text-align: center;
+    display:flex;
+    width:100%;
+    align-items: center;
+}
+.day .base{
+    width: 100%;
+}
+.day .extra{
+    display:none;
+}
+:host(#show) .day .base{
+    width:50%;
+}
+:host(#show) .day .extra{
+    display: block;
+    width:50%;
+}
+.day p{
+    margin:0.25em;
+    font-size:0.9em;
+    text-align: left;
+    font-weight: 300;
+}
+.day p#date{
+    text-align:center;
+    font-size:1.2em;
+    margin-top: -5%;
+}
+.day img{
+    width:100%;
+    height:auto;
+}
+.day img.icon{
+    width:30px;
+    margin-right:5px;
+}
+.day .extra p{
+    display:flex;
+    align-items: center;
+}
+@media(max-width: 1000px){
+    .day p#date{
+        font-size:0.9em;
+    }
+}
+@media(max-width:367px){
+    .five daily-forecast:not(#show):nth-child(3),
+    .five daily-forecast:not(#show):nth-child(4),
+    .five daily-forecast:not(#show):nth-child(5),
+    .five daily-forecast:not(#show):nth-child(6)
+    {
+        width:33.3%;
+    }
+    .five daily-forecast:not(#show):first-child,
+    .five daily-forecast:not(#show):nth-child(2),
+    .five daily-forecast#show~daily-forecast:not(#show):nth-child(3)
+    {
+        width:50%;
+    }
+}
+@media(max-width:300px){
+    .btn-group{
+        font-size:0.9em;
+    }
+}
+</style>
 <div class="btn-group">
     <button class="active" id="current">Heute</button>
     <button id="three">3-Tage</button>
@@ -12,16 +141,13 @@ tmpl.innerHTML = `
 <div id="weather">
 </div>`;
 
-
 function getWeekdayName(number){
     let weekdays = ["SO", "MO", "DI", "MI", "DO", "FR", "SA"];
     return weekdays[number];
 }
-
 class Weather extends HTMLElement {
     constructor() {
         super();
-        console.log("Wetter Component registered!");
         const shadowRoot = this.attachShadow({mode: 'open'});
         shadowRoot.appendChild(tmpl.content.cloneNode(true));
     }
@@ -40,23 +166,33 @@ class Weather extends HTMLElement {
         this.shadowRoot.querySelector("#eight").onclick=(e) =>this.updateDays(e);
     }
     get lat(){
-        return this.getAttribute("lat");
-    }
+        return this.getAttribute("lat");}
+    set lat(val){
+        this.setAttribute("lat",val);}
     get lon(){
-        return this.getAttribute("lon");
-    }
+        return this.getAttribute("lon");}
+    set lon(val){
+        this.setAttribute("lon",val);}
+    get iconBase(){
+        return this.getAttribute("iconBase")||"https://web-components-bachelor.netlify.app/Components/Wetter/"}
+    set iconBase(val){
+        this.setAttribute("iconBase",val);}
     get apikey(){
-        return this.getAttribute("apikey");
-    }
+        return this.getAttribute("apikey");}
+    set apikey(val){
+        this.setAttribute("apikey",val);}
     get units(){
-        return this.getAttribute("units") || "metric";
-    }
+        return this.getAttribute("units") || "metric";}
+    set units(val){
+        this.setAttribute("units",val);}
     get lang(){
-        return this.getAttribute("lang") || "en";
-    }
+        return this.getAttribute("lang") || "en";}
+    set lang(val){
+        this.setAttribute("lang",val);}
     get exclude(){
-        return this.getAttribute("exclude")?this.getAttribute("exclude").split(","): [];
-    }
+        return this.getAttribute("exclude") || [];}
+    set exclude(val){
+        this.setAttribute("exclude",val);}
     updateDays(e){
         switch(e.target.getAttribute('id')){
             case "current": this.days = 1; break;
@@ -69,33 +205,14 @@ class Weather extends HTMLElement {
         this.shadowRoot.getElementById("weather").setAttribute("class",e.target.getAttribute("id"));
         this.updateWeather()
     }
-
     updateWeather(){
         let children = this.shadowRoot.getElementById("weather").querySelectorAll("daily-forecast:not(#show)")
         for (let child = children.length-1; child >= this.days ; child --){
             children[child].remove();
         }
-
         if(children.length < this.days){
             this.loadWeather();
         }
-        /*children = this.shadowRoot.getElementById("weather").querySelectorAll("daily-forecast:not(#show)");
-        for(let i =0; i < children.length; i++){
-            children[i].animate([{
-                transform: 'translateY(30px)',
-                opacity:'50%'
-            }, {
-                transform: 'translateY(0px)',
-                opacity: '100%'
-            }
-
-            ], {
-                duration: 1000,
-                iterations: 1,
-                delay: 50*i
-            })
-        }*/
-
         if(!this.shadowRoot.querySelector("#show") || !this.shadowRoot.querySelector(".chosen")){
             if(this.shadowRoot.querySelector(".chosen")){
                 this.shadowRoot.querySelector(".chosen").removeAttribute("class");
@@ -108,10 +225,8 @@ class Weather extends HTMLElement {
             first.setAttribute("class","chosen");
             clone.setAttribute("id","show");
             this.shadowRoot.getElementById("weather").appendChild(clone);
-
         }
     }
-
     loadWeather(){
         if(this.response){
             for(let i =0; i < this.days; i ++){
@@ -126,6 +241,7 @@ class Weather extends HTMLElement {
                     forecast.setAttribute("icon", this.response[i].weather[0].icon);
                     forecast.setAttribute("weather-text", this.response[i].weather[0].description);
                     forecast.setAttribute("date", this.response[i].dt);
+                    forecast.setAttribute("iconBase", this.iconBase);
 
                     if(!this.exclude.includes("temperature")){
                         forecast.setAttribute("temp-min", this.response[i].temp.min);
@@ -143,7 +259,6 @@ class Weather extends HTMLElement {
                         forecast.setAttribute("sunrise", this.response[i].sunrise);
                         forecast.setAttribute("sunset", this.response[i].sunset);
                     }
-
                     forecast.onclick=(e)=>{
                         this.shadowRoot.querySelector(".chosen").removeAttribute("class");
                         this.shadowRoot.getElementById("show").remove();
@@ -152,7 +267,6 @@ class Weather extends HTMLElement {
                         clone.setAttribute("id","show");
                         this.shadowRoot.getElementById("weather").appendChild(clone);
                     };
-
                     this.shadowRoot.getElementById("weather").appendChild(forecast);
                 }
             }
@@ -160,24 +274,149 @@ class Weather extends HTMLElement {
     }
 }
 window.customElements.define('weather-forecast', Weather);
-
 let weather = document.createElement('template');
-weather.innerHTML = `<link rel="stylesheet" type="text/css" href="`+ baseURL + `Wetter.css" />
+weather.innerHTML = `
+<style>:host{
+    max-width: 600px;
+    display:block;
+    margin-left:auto;
+    margin-right:auto;
+}
+#weather{
+    display:flex;
+    position: relative;
+    flex-wrap: wrap;
+    background:var(--background,#fff);
+}
+.btn-group{
+    display: flex;
+    justify-content: space-between;
+}
+.btn-group button{
+    border:none;
+    width:100%;
+    padding:1em;
+    font-family: Lato, sans-serif;
+    background:var(--background-deactived, #f2f2f2)
+}
+.btn-group button.active{
+    background: var(--background,#fff);
+}
+daily-forecast{
+    background: var(--background,#fff);
+    padding:5%;
+    box-sizing: border-box;
+    display:flex;
+    justify-content: center;
+    order:2;
+}
+daily-forecast:not(#show){
+    cursor:pointer;
+}
+daily-forecast.chosen{
+    background:var(--background-chosen, #dff0ff);
+}
+.three daily-forecast:not(#show)
+{
+    width:33.3%;
+}
+.five daily-forecast:not(#show)
+{
+    width:20%;
+}
+.eight daily-forecast:not(#show)
+{
+    width:25%;
+}
+.current daily-forecast:not(#show){
+    display:none;
+}
+daily-forecast#show{
+    order:1;
+    width:100%!important;
+    height:auto;
+}
+.day{
+    text-align: center;
+    display:flex;
+    width:100%;
+    align-items: center;
+}
+.day .base{
+    width: 100%;
+}
+.day .extra{
+    display:none;
+}
+:host(#show) .day .base{
+    width:50%;
+}
+:host(#show) .day .extra{
+    display: block;
+    width:50%;
+}
+.day p{
+    margin:0.25em;
+    font-size:0.9em;
+    text-align: left;
+    font-weight: 300;
+}
+.day p#date{
+    text-align:center;
+    font-size:1.2em;
+    margin-top: -5%;
+}
+.day img{
+    width:100%;
+    height:auto;
+}
+.day img.icon{
+    width:30px;
+    margin-right:5px;
+}
+.day .extra p{
+    display:flex;
+    align-items: center;
+}
+@media(max-width: 1000px){
+    .day p#date{
+        font-size:0.9em;
+    }
+}
+@media(max-width:367px){
+    .five daily-forecast:not(#show):nth-child(3),
+    .five daily-forecast:not(#show):nth-child(4),
+    .five daily-forecast:not(#show):nth-child(5),
+    .five daily-forecast:not(#show):nth-child(6)
+    {
+        width:33.3%;
+    }
+    .five daily-forecast:not(#show):first-child,
+    .five daily-forecast:not(#show):nth-child(2),
+    .five daily-forecast#show~daily-forecast:not(#show):nth-child(3)
+    {
+        width:50%;
+    }
+}
+@media(max-width:300px){
+    .btn-group{
+        font-size:0.9em;
+    }
+}</style>
 <div class="day">
 <div class="base">
     <img  id="icon"/>
     <p id="date">No Data</p>
 </div>
 <div class="extra">
-    <p id="weather-text"><img class="icon" id="text-icon" src="`+ baseURL + `rain.svg"/><span>No Data</span></p>
-    <p id="temperature"><img class="icon" id="temp-icon" src="`+ baseURL + `temperature-warm.svg"/><span>No Data</span></p>
-    <p id="rain"><img class="icon" src="`+ baseURL + `rain.svg"/><span>No Data</span></p>
-    <p id="wind"><img class="icon" src="`+ baseURL + `wind.svg"/><span>No Data</span></p>
-    <p id="sunrise"><img class="icon" src="`+ baseURL + `sunrise.svg"/><span>No Data</span></p>
-    <p id="sunset"><img class="icon" src="`+ baseURL + `sunset.svg"/><span>No Data</span></p>
+    <p id="weather-text"><img class="icon" id="text-icon" src="rain.svg"/><span>No Data</span></p>
+    <p id="temperature"><img class="icon" id="temp-icon" alt="Warm Temperature Icon" src="temperature-warm.svg"/><span>No Data</span></p>
+    <p id="rain"><img class="icon" alt="Rain Icon" src="rain.svg"/><span>No Data</span></p>
+    <p id="wind"><img class="icon" alt="Wind Icon" src="wind.svg"/><span>No Data</span></p>
+    <p id="sunrise"><img class="icon" alt="Sunrise Icon" src="sunrise.svg"/><span>No Data</span></p>
+    <p id="sunset"><img class="icon" alt="Sunset Icon" src="sunset.svg"/><span>No Data</span></p>
 </div>
 </div>`;
-
 class Daily extends HTMLElement {
     constructor() {
         super();
@@ -189,14 +428,11 @@ class Daily extends HTMLElement {
     }
 
     get date(){
-        return this.getAttribute("date")||" ";
-    }
+        return this.getAttribute("date")||" ";}
     get icon(){
-        return this.getAttribute("icon")||" ";
-    }
+        return this.getAttribute("icon")||" ";}
     get weatherText(){
-        return this.getAttribute("weather-text")|| " ";
-    }
+        return this.getAttribute("weather-text")|| " ";}
     get temperature(){
         if(this.getAttribute("temp-min") && this.getAttribute("temp-max")){
             return {min: this.getAttribute("temp-min"),
@@ -223,7 +459,15 @@ class Daily extends HTMLElement {
                 sunset: this.getAttribute("sunset")};
         }else return undefined;
     }
-
+    get iconBase(){
+        return this.getAttribute("iconBase");
+    }
+    setImages(){
+        this.shadowRoot.querySelector("#rain img").setAttribute("src",this.iconBase+"rain.svg");
+        this.shadowRoot.querySelector("#wind img").setAttribute("src",this.iconBase+"wind.svg");
+        this.shadowRoot.querySelector("#sunrise img").setAttribute("src",this.iconBase+"sunrise.svg");
+        this.shadowRoot.querySelector("#sunset img").setAttribute("src",this.iconBase+"sunset.svg");
+    }
     setDate(){
         let dt = new Date(parseInt(this.date)*1000);
         this.shadowRoot.getElementById("date").innerText= getWeekdayName(dt.getDay());
@@ -232,20 +476,24 @@ class Daily extends HTMLElement {
         this.shadowRoot.querySelector("#weather-text span").innerText=this.weatherText;
     }
     setIcon(){
-        this.shadowRoot.getElementById("icon").setAttribute("src", baseURL + this.icon+".png");
-        this.shadowRoot.getElementById("text-icon").setAttribute("src", baseURL + this.icon+".png");
+        this.shadowRoot.getElementById("icon").setAttribute("src", this.iconBase + this.icon+".png");
+        this.shadowRoot.getElementById("icon").setAttribute("alt", this.weatherText);
+
+        this.shadowRoot.getElementById("text-icon").setAttribute("src", this.iconBase + this.icon+".png");
+        this.shadowRoot.getElementById("text-icon").setAttribute("alt", this.weatherText);
     }
     setTemp(){
-        console.log(this.temperature);
         this.shadowRoot.querySelector("#temperature span").innerText=this.temperature.min+"° / "+ this.temperature.max + "°";
         if(parseFloat(this.temperature.min)<=10){
-            this.shadowRoot.getElementById("temp-icon").setAttribute("src",baseURL+"temperature-cold.svg");
+            this.shadowRoot.getElementById("temp-icon").setAttribute("src",this.iconBase+"temperature-cold.svg");
+            this.shadowRoot.getElementById("temp-icon").setAttribute("alt","Cold Temperature Icon");
         }else if(parseFloat(this.temperature.max)>=25){
-            this.shadowRoot.getElementById("temp-icon").setAttribute("src",baseURL+"temperature-hot.svg");
+            this.shadowRoot.getElementById("temp-icon").setAttribute("src",this.iconBase+"temperature-hot.svg");
+            this.shadowRoot.getElementById("temp-icon").setAttribute("alt","Hot Temperature Icon");
         }else{
-            this.shadowRoot.getElementById("temp-icon").setAttribute("src",baseURL+"temperature-warm.svg");
+            this.shadowRoot.getElementById("temp-icon").setAttribute("src",this.iconBase+"temperature-warm.svg");
+            this.shadowRoot.getElementById("temp-icon").setAttribute("alt","Warm Temperature Icon");
         }
-
     }
     setRain(){
         let raintext = this.rain.pop + "%";
@@ -264,6 +512,7 @@ class Daily extends HTMLElement {
         this.shadowRoot.querySelector("#sunset span").innerText= suns.getHours() + ":" + (suns.getMinutes() <10? "0":"")+ suns.getMinutes() ;
     }
     connectedCallback(){
+        this.setImages();
         this.setIcon();
         this.setDate();
         this.setText();
@@ -312,7 +561,6 @@ class Daily extends HTMLElement {
             this.setSun();
         }
     }
-
 }
 window.customElements.define('daily-forecast', Daily);
 
